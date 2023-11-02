bd054b51c80dcd91720d32783230295d891484ec
Grey <gsnearing@google.com>
2023-11-02 20:59:09 +0000

    Code after paper revisou #1

--- notebooks/backend/gauge_groups_utils.py
+++ notebooks/backend/gauge_groups_utils.py
@@ -13,51 +13,51 @@ from backend import data_paths
 def load_gauge_group(
     gauge_group_path: pathlib.Path
 ) -> list[str]:
-  """Loads and gauge group file."""
+    """Loads and gauge group file."""
 
-  with open(gauge_group_path, 'rt') as f:
-    lines = f.readlines()
+    with open(gauge_group_path, 'rt') as f:
+        lines = f.readlines()
 
-  gauges = [gauge.strip('\n') for gauge in lines]
+    gauges = [gauge.strip('\n') for gauge in lines]
 
-  return gauges
+    return gauges
 
 
 def write_gauge_group(filename: str, basins: list[str]):
-  with open(filename, 'wt') as f:
-    for basin in basins:
-      f.write("%s\n" % basin)
+    with open(filename, 'wt') as f:
+        for basin in basins:
+            f.write("%s\n" % basin)
 
 def get_full_gauge_group() -> list[str]:
-  """Returns the gauge group will all gauges used for the paper."""
-  return load_gauge_group(data_paths.FULL_GAUGE_GROUP_FILE)
+    """Returns the gauge group will all gauges used for the paper."""
+    return load_gauge_group(data_paths.FULL_GAUGE_GROUP_FILE)
 
 
 def load_experiment_gauge_groups(
     experiment_dir_name: str
 ) -> dict[str, list[str]]:
-  """Loads a dictionary of gauge groups for a split-sample experiment."""
- 
-  gauge_groups_dir = data_paths.GAUGE_GROUPS_DIR / experiment_dir_name
-  gauge_group_paths = glob.glob(str(gauge_groups_dir / '*'))
+    """Loads a dictionary of gauge groups for a split-sample experiment."""
 
-  print(f'Working on {experiment_dir_name} ...')
+    gauge_groups_dir = data_paths.GAUGE_GROUPS_DIR / experiment_dir_name
+    gauge_group_paths = glob.glob(str(gauge_groups_dir / '*'))
 
-  experiment_gauge_groups = {}
-  for path in tqdm.tqdm(gauge_group_paths):
-    split_name = pathlib.Path(path).stem
-    experiment_gauge_groups[split_name] = load_gauge_group(
-        gauge_group_path=path)
- 
-  return experiment_gauge_groups
+    print(f'Working on {experiment_dir_name} ...')
+
+    experiment_gauge_groups = {}
+    for path in tqdm.tqdm(gauge_group_paths):
+        split_name = pathlib.Path(path).stem
+        experiment_gauge_groups[split_name] = load_gauge_group(
+            gauge_group_path=path)
+
+    return experiment_gauge_groups
 
 
 def load_all_ungauged_gauge_groups() -> dict[str, dict[str, list[str]]]:
-  """Loads gauge groups for all ungauged experiments."""
-  return {
-      experiment: load_experiment_gauge_groups(
-          experiment_dir_name=experiment) 
-      for experiment in data_paths.UNGAUGED_EXPERIMENTS
-  }
+    """Loads gauge groups for all ungauged experiments."""
+    return {
+        experiment: load_experiment_gauge_groups(
+            experiment_dir_name=experiment) 
+        for experiment in data_paths.UNGAUGED_EXPERIMENTS
+    }
 
 
--- notebooks/backend/skill_prediction_utils.py
+++ notebooks/backend/skill_prediction_utils.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -8,8 +22,10 @@ from sklearn.metrics import precision_recall_fscore_support
 import tqdm
 from typing import Optional
 
-N_KFOLD = 5
-N_TREES = 20
+from backend import evaluation_utils
+
+N_KFOLD = 25
+N_TREES = 100
 
 
 def make_predictors(
@@ -19,22 +35,22 @@ def make_predictors(
     bins: Optional[list[int]] = None,
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
 
-  if bins is not None:
-    bin_names = [idx for idx, bin in enumerate(bins[1:])]
-    y_cat = pd.cut(y, bins=bins, include_lowest=True, labels=bin_names)
-  else:
-    y_cat = y
+    if bins is not None:
+        bin_names = [idx for idx, bin in enumerate(bins[1:])]
+        y_cat = pd.cut(y, bins=bins, include_lowest=True, labels=bin_names)
+    else:
+        y_cat = y
 
-  joined = pd.concat([x, y_cat], axis=1).dropna(axis=0)
-  joined.replace([np.inf, -np.inf], np.nan, inplace=True)
-  joined = joined.dropna()
-  predictand = joined[metric]
-  predictors = joined.drop(metric, axis=1)
+    joined = pd.concat([x, y_cat], axis=1).dropna(axis=0)
+    joined.replace([np.inf, -np.inf], np.nan, inplace=True)
+    joined = joined.dropna()
+    predictand = joined[metric]
+    predictors = joined.drop(metric, axis=1)
 
-  print(f'There are {predictors.shape[1]} predictors.')
-  print(f'There are {predictors.shape[0]} samples.')
+    print(f'There are {predictors.shape[1]} predictors.')
+    print(f'There are {predictors.shape[0]} samples.')
 
-  return predictors, predictand
+    return predictors, predictand
 
 
 def train_kfold(
@@ -44,55 +60,53 @@ def train_kfold(
     regression: bool = False
 ):
 
-  if regression and classifier:
-    raise ValueError('Can only use regression *or* classifier, got both.')
-  if not regression and not classifier:
-    raise ValueError('Must use either regression or classifier.')
+    if regression and classifier:
+        raise ValueError('Can only use regression *or* classifier, got both.')
+    if not regression and not classifier:
+        raise ValueError('Must use either regression or classifier.')
 
-  # Initialize storage.
-  basins = list(predictand.index)
-  y_hat = pd.Series(index=basins)
+    # Initialize storage.
+    basins = list(predictand.index)
+    y_hat = pd.Series(index=basins)
 
-  # Create a separate split for each ensemble member.
+    # Create a separate split for each ensemble member.
+    if classifier:
+        kf = model_selection.StratifiedKFold(
+            n_splits=N_KFOLD,
+            random_state=None,
+            shuffle=True
+        )
+        splits = kf.split(basins, predictand)
+    elif regression:
+        kf = model_selection.KFold(
+            n_splits=N_KFOLD,
+            random_state=None,
+            shuffle=True
+        )
+        splits = kf.split(basins)
 
-  if classifier:
-    kf = model_selection.StratifiedKFold(
-        n_splits=N_KFOLD,
-        random_state=None,
-        shuffle=True
-    )
-    splits = kf.split(basins, predictand)
-  elif regression:
-    kf = model_selection.KFold(
-        n_splits=N_KFOLD,
-        random_state=None,
-        shuffle=True
-    )
-    splits = kf.split(basins)
+    # Train and test the model.
+    for kfold, (train_index, test_index) in enumerate(tqdm.tqdm(splits)):
+        print(f'Training fold # {kfold}')
 
-  # Train and test the model.
-  for kfold, (
-      train_index, test_index) in enumerate(tqdm.tqdm(splits)):
-    print(f'Training fold # {kfold}')
+        # Train/test split.
+        train_basins = [basins[idx] for idx in train_index]
+        test_basins = [basins[idx] for idx in test_index]
+        train_y = predictand.loc[train_basins]
+        train_x = predictors.loc[train_y.index]
+        test_x = predictors.loc[test_basins]
 
-    # Train/test split.
-    train_basins = [basins[idx] for idx in train_index]
-    test_basins = [basins[idx] for idx in test_index]
-    train_y = predictand.loc[train_basins]
-    train_x = predictors.loc[train_y.index]
-    test_x = predictors.loc[test_basins]
+        # RF model.
+        if regression:
+            rf = RandomForestRegressor(n_estimators=N_TREES, random_state=42)
+        elif classifier:
+            rf = RandomForestClassifier(n_estimators=N_TREES, random_state=42, class_weight='balanced')
+        rf.fit(train_x, train_y)
 
-    # RF model.
-    if regression:
-      rf = RandomForestRegressor(n_estimators=N_TREES, random_state=42)
-    elif classifier:
-      rf = RandomForestClassifier(n_estimators=N_TREES, random_state=42, class_weight='balanced')
-    rf.fit(train_x, train_y)
+        # Predictions.
+        y_hat.loc[test_basins] = rf.predict(test_x)
 
-    # Predictions.
-    y_hat.loc[test_basins] = rf.predict(test_x)
-
-  return y_hat
+    return y_hat
 
 
 def feature_importances(
@@ -102,44 +116,58 @@ def feature_importances(
     regression: bool = False
 ):
 
-  if regression and classifier:
-    raise ValueError('Can only use regression *or* classifier, got both.')
-  if not regression and not classifier:
-    raise ValueError('Must use either regression or classifier.')
+    if regression and classifier:
+        raise ValueError('Can only use regression *or* classifier, got both.')
+    if not regression and not classifier:
+        raise ValueError('Must use either regression or classifier.')
 
-  # Train a predictor with all of the data.
-  if regression:
-    rf = RandomForestRegressor(n_estimators=N_TREES, random_state=42)
-  elif classifier:
-    rf = RandomForestClassifier(n_estimators=N_TREES, random_state=42)
-  rf.fit(predictors, predictand)
+    # Train a predictor with all of the data.
+    if regression:
+        rf = RandomForestRegressor(n_estimators=N_TREES, random_state=42)
+    elif classifier:
+        rf = RandomForestClassifier(n_estimators=N_TREES, random_state=42)
+    rf.fit(predictors, predictand)
 
-  # Extract importances.
-  importances = pd.Series(
-      index=predictors.columns, data=rf.feature_importances_
-  ).sort_values(ascending=False)
-  importance_stds = np.std(
-      [tree.feature_importances_ for tree in rf.estimators_], axis=0
-  )
+    # Extract importances.
+    importances = pd.Series(
+        index=predictors.columns, data=rf.feature_importances_
+    ).sort_values(ascending=False)
+    importance_stds = np.std(
+        [tree.feature_importances_ for tree in rf.estimators_], axis=0
+    )
 
-  return importances, importance_stds
+    return importances, importance_stds
 
 
 def plot_feature_importances(
     importances: pd.DataFrame,
     ax: plt.Axes
 ):
-  importances = importances.sort_values(ascending=False)
-  ax.bar(importances.index, importances.values)
-  ax.set_title('Feature Importances', fontsize=20)
-  ax.set_ylabel('Mean Decrease in Impurity', fontsize=14)
-  ax.grid(c='#EEE')
-  ax.set_xticks(
-      range(len(importances.index)),
-      importances.index,
-      rotation=60,
-      ha='right',
-  )
-  ax.set_axisbelow(True)
-
-
+    importances = importances.sort_values(ascending=False)
+    ax.bar(importances.index, importances.values)
+    ax.set_title(
+        'Feature Importances',
+        fontsize=evaluation_utils.NATURE_FONT_SIZES['title']
+    )
+    ax.set_ylabel(
+        'Mean Decrease in Impurity', 
+        fontsize=evaluation_utils.NATURE_FONT_SIZES['axis_label']
+    )
+    ax.grid(c='#EEE')
+    ax.set_xticks(
+        range(len(importances.index)),
+        importances.index,
+        rotation=60,
+        ha='right',
+        fontsize=evaluation_utils.NATURE_FONT_SIZES['tick_labels']
+    )
+    ax.set_yticklabels(
+        ax.get_yticklabels(),
+        fontsize=evaluation_utils.NATURE_FONT_SIZES['tick_labels']
+    )
+    ax.set_xlabel(None)
+    ax.set_ylabel(
+        ax.get_ylabel(), 
+        fontsize=evaluation_utils.NATURE_FONT_SIZES['axis_label']
+    )
+    ax.set_axisbelow(True)
